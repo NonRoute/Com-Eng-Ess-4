3f4a56c23b05bd662ae7a443c80ee6bd291dc198
Chanon Rattanajaratkul <>
2021-05-03 12:19:56 +0000

    Test change from replit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDZSDeBS6BpfcNoVkrWfx-uNeLZMscOo1o",
+    authDomain: "projectengess.firebaseapp.com",
+    projectId: "projectengess",
+    storageBucket: "projectengess.appspot.com",
+    messagingSenderId: "434969956116",
+    appId: "1:434969956116:web:f5d7ac191e382b821d3c01"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 function on() {
     document.getElementById("overlay").style.display = "block";
 }
@@ -10,7 +21,6 @@ async function autoGenerate() {
     //https://github.com/lukePeavey/quotable#get-random-quote
     const response = await fetch('https://api.quotable.io/random')
     const data = await response.json()
-    console.log(data.content);
     document.getElementById("yw").value = data.content;
     document.getElementById("pn").value = data.author;
 }
